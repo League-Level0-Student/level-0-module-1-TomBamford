64e1549f0938a814add603d9cfc549ada0e2ab66
root <root@71966730b4cd>
2020-07-11 21:29:38 +0000

    Day 3

---
+++ src/_03_if_else/_1_unbirthday/unbirthday.java
@@ -0,0 +1,16 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+public static void main(String[] args) {
+	String input = JOptionPane.showInputDialog("What is your birthday? (mm/dd)");
+	if(input.equals("03/20")) {
+		JOptionPane.showMessageDialog(null, "Happy Birthday!");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "Happy Unbirthday!");
+	}
+	
+}
+}
--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,11 +8,22 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+		String ex1 = "He is kind";
+		String ex2 = "He is smart";
+		String ex3 = "She is hardworking";
+				
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+		String input = JOptionPane.showInputDialog("Enter a name out of the following (ex1,ex2,ex3)");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+		if(input.equals("ex1")) {
+			JOptionPane.showMessageDialog(null, ex1);
+		}
+		if(input.equals("ex2")) {
+			JOptionPane.showMessageDialog(null, ex2);
+		}
+		if(input.equals("ex3")) {
+			JOptionPane.showMessageDialog(null, ex3);
+		}
 	}
 }
 
